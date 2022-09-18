0b3976f66914cf29ac319d64f95f7da53804aac3
PhuongNamHoang198 <hoangphuongnam.98@gmail.com>
2022-09-18 09:56:03 +0700

    add chu thich

--- src/main/java/controller/EmailController.java
+++ src/main/java/controller/EmailController.java
@@ -14,10 +14,28 @@ public class EmailController {
     private static final String EMAIL_REGEX = "^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
     private static Pattern pattern;
     private Matcher matcher;
+    // matcher dung de so sanh voi regex
+    // pattern dung de luu tru regex
+    // regex dung de kiem tra email
+    //
 
-    public EmailController() {
+//pattern có những phương thức nào:
+//    private int flags;
+//    private String pattern;
+
+    public EmailController()
+    // ham nay dung de kiem tra email
+//
+    {
       pattern = Pattern.compile(EMAIL_REGEX);
 
+    }
+    private boolean validate(String regex)
+    //phương thức matcher có những phương thức nào: //
+
+    {
+        matcher = pattern.matcher(regex);
+        return matcher.matches();
     }
     @GetMapping("/")
     String getIndex() {
@@ -28,14 +46,11 @@ public class EmailController {
     String validateEmail (@RequestParam("email") String email, Model model) {
         boolean isValid = this.validate(email);
         if (!isValid) {
-            model.addAttribute("message" ,"Email is invalid");
+            model.addAttribute("message" ,"Email is invalid-lam gi co email nay");
             return "index";
         }
         model.addAttribute("email", email);
         return "success";
     }
-    private boolean validate(String regex) {
-        matcher = pattern.matcher(regex);
-        return matcher.matches();
-    }
+
 }
